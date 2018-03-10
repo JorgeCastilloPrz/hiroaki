f66311460bfe15d1881c62d8c6c8fb271761395a
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-03-10 20:01:15 +0100

    add nested object to double check parsing logic

--- app/src/main/java/com/jorgecastillo/hiroaki/model/Article.kt
+++ app/src/main/java/com/jorgecastillo/hiroaki/model/Article.kt
@@ -1,9 +1,12 @@
 package com.jorgecastillo.hiroaki.model
 
 data class Article(
-    val title: String,
-    val description: String,
-    val url: String,
-    val urlToImage: String,
-    val publishedAt: String
+        val title: String,
+        val description: String,
+        val url: String,
+        val urlToImage: String,
+        val publishedAt: String,
+        val source: Source
 )
+
+data class Source(val id: String?, val name: String)
