b6edee542098e8bd3a7e8213a36b7715abad8eb0
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-03-11 17:08:49 +0100

    add jackson use case test suite

--- app/src/main/java/com/jorgecastillo/hiroaki/data/networkdto/GsonArticleDto.kt
+++ app/src/main/java/com/jorgecastillo/hiroaki/data/networkdto/GsonArticleDto.kt
@@ -7,7 +7,7 @@ import com.jorgecastillo.hiroaki.model.Source
 data class GsonNewsResponse(
         @SerializedName("status") val status: String,
         @SerializedName("totalResults") val totalResults: Long,
-        @SerializedName("articles") val articles: List<MoshiArticleDto>
+        @SerializedName("articles") val articles: List<GsonArticleDto>
 )
 
 data class GsonArticleDto(
@@ -16,7 +16,7 @@ data class GsonArticleDto(
         @SerializedName("url") val url: String,
         @SerializedName("urlToImage") val urlToImage: String,
         @SerializedName("publishedAt") val publishedAt: String,
-        @SerializedName("source") val source: MoshiSourceDto
+        @SerializedName("source") val source: GsonSourceDto
 )
 
 data class GsonSourceDto(
@@ -25,7 +25,7 @@ data class GsonSourceDto(
 
 fun GsonArticleDto.toArticle() = Article(title, description, url, urlToImage, publishedAt, source.toSource())
 
-fun Article.toGsonDto() = GsonArticleDto(title, description, url, urlToImage, publishedAt, source.toMoshiDto())
+fun Article.toGsonDto() = GsonArticleDto(title, description, url, urlToImage, publishedAt, source.toGsonDto())
 
 fun GsonSourceDto.toSource() = Source(id, name)
 
---
+++ app/src/main/java/com/jorgecastillo/hiroaki/data/service/JacksonNewsApiService.kt
@@ -0,0 +1,21 @@
+package com.jorgecastillo.hiroaki.data.service
+
+import com.jorgecastillo.hiroaki.data.networkdto.JacksonArticleDto
+import com.jorgecastillo.hiroaki.data.networkdto.JacksonNewsResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+interface JacksonNewsApiService {
+    @GET("/v2/top-headlines?sources=crypto-coins-news&apiKey=a7c816f57c004c49a21bd458e11e2807")
+    @Headers("Cache-Control: max-age=640000")
+    fun getNews(): Call<JacksonNewsResponse>
+
+    /**
+     * This is a no-op method on the real news API, just created for testing purposes.
+     */
+    @POST("/v2/top-headlines?apiKey=a7c816f57c004c49a21bd458e11e2807")
+    fun publishHeadline(@Body articleDto: JacksonArticleDto): Call<Void>
+}
