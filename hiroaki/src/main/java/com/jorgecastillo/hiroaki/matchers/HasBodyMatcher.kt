a367f81c42f21de87a8b45c9e5cbdb31bd5fd749
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-03-11 12:52:53 +0100

    rename params on hasBody matcher

--- hiroaki/src/main/java/com/jorgecastillo/hiroaki/matchers/HasBodyMatcher.kt
+++ hiroaki/src/main/java/com/jorgecastillo/hiroaki/matchers/HasBodyMatcher.kt
@@ -1,6 +1,5 @@
 package com.jorgecastillo.hiroaki.matchers
 
-import com.jorgecastillo.hiroaki.parse
 import okhttp3.mockwebserver.RecordedRequest
 import org.hamcrest.Description
 import org.hamcrest.Matcher
@@ -9,10 +8,8 @@ import org.hamcrest.TypeSafeMatcher
 /**
  * Custom Hamcrest matcher to assert about the outgoing body of an OkHttp RecordedRequest.
  */
-fun <T> hasBody(stringBody: String, body: T, parsedRequestBody: Pair<T, String>):
+fun <T> hasBody(stringBody: String, parsedExpectedBody: T, parsedRequestBody: Pair<T, String>):
         Matcher<RecordedRequest> = object : TypeSafeMatcher<RecordedRequest>() {
-
-    var requestBodySnapshot: String = ""
 
     override fun describeTo(description: Description) {
         description.appendText("The HTTP query should have the following body: ")
@@ -21,11 +18,10 @@ fun <T> hasBody(stringBody: String, body: T, parsedRequestBody: Pair<T, String>)
 
     override fun describeMismatchSafely(request: RecordedRequest,
                                         mismatchDescription: Description) {
-        mismatchDescription.appendText("\n$requestBodySnapshot")
+        mismatchDescription.appendText("\n${parsedRequestBody.second}")
     }
 
     override fun matchesSafely(request: RecordedRequest): Boolean {
-        requestBodySnapshot = parsedRequestBody.second
-        return parsedRequestBody.first == body
+        return parsedRequestBody.first == parsedExpectedBody
     }
 }
