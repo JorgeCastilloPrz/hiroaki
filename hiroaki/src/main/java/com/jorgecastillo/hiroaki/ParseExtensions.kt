6b21db4ab707ff10e0b3e6614444c43445f7d79a
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-03-11 12:42:25 +0100

    use parsers to type and not to string

---
+++ hiroaki/src/main/java/com/jorgecastillo/hiroaki/ParseExtensions.kt
@@ -0,0 +1,24 @@
+package com.jorgecastillo.hiroaki
+
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonParseException
+import com.google.gson.reflect.TypeToken
+import okhttp3.mockwebserver.RecordedRequest
+import java.io.File
+
+@Throws(Exception::class)
+fun <T : Any> T.fileContentAsString(fileName: String): String {
+    val classLoader = this::class.java.classLoader
+    val file = File(classLoader.getResource(fileName).file)
+    return file.readText(Charsets.UTF_8)
+}
+
+@Throws(Exception::class)
+fun <T> String.fromJson(clazz: Class<T>): T =
+        GsonBuilder().create().fromJson(this, clazz)
+
+@Throws(JsonParseException::class)
+fun <T> RecordedRequest.parse(clazz: Class<T>): Pair<T, String> {
+    val bodyString = this.body.readUtf8()
+    return Pair(GsonBuilder().create().fromJson(bodyString, clazz), bodyString)
+}
--- hiroaki/src/main/java/com/jorgecastillo/hiroaki/matchers/HasBodyMatcher.kt
+++ hiroaki/src/main/java/com/jorgecastillo/hiroaki/matchers/HasBodyMatcher.kt
@@ -1,6 +1,6 @@
 package com.jorgecastillo.hiroaki.matchers
 
-import com.jorgecastillo.hiroaki.bodyAsMap
+import com.jorgecastillo.hiroaki.parse
 import okhttp3.mockwebserver.RecordedRequest
 import org.hamcrest.Description
 import org.hamcrest.Matcher
@@ -9,39 +9,23 @@ import org.hamcrest.TypeSafeMatcher
 /**
  * Custom Hamcrest matcher to assert about the outgoing body of an OkHttp RecordedRequest.
  */
-fun hasBody(body: Map<String, String>): Matcher<RecordedRequest> {
-    var requestBodySnapshot: Map<String, String> = mapOf()
+fun <T> hasBody(stringBody: String, body: T, parsedRequestBody: Pair<T, String>):
+        Matcher<RecordedRequest> = object : TypeSafeMatcher<RecordedRequest>() {
 
-    return object : TypeSafeMatcher<RecordedRequest>() {
+    var requestBodySnapshot: String = ""
 
-        override fun describeTo(description: Description) {
-            description.appendText("The HTTP query should have the following body: ")
-            description.appendText("\n$body")
-        }
+    override fun describeTo(description: Description) {
+        description.appendText("The HTTP query should have the following body: ")
+        description.appendText("\n$stringBody")
+    }
 
-        override fun describeMismatchSafely(request: RecordedRequest,
-                                            mismatchDescription: Description) {
-            body.keys.forEach { key ->
-                if (requestBodySnapshot[key] == null) {
-                    mismatchDescription.appendText("\n The key $key is not present.")
-                } else {
-                    if (requestBodySnapshot[key] != body[key]) {
-                        mismatchDescription.appendText(
-                                "\n$key = ${requestBodySnapshot[key]} >>> Not matching!")
-                    }
-                }
-            }
-        }
+    override fun describeMismatchSafely(request: RecordedRequest,
+                                        mismatchDescription: Description) {
+        mismatchDescription.appendText("\n$requestBodySnapshot")
+    }
 
-        override fun matchesSafely(request: RecordedRequest): Boolean {
-            requestBodySnapshot = request.bodyAsMap()
-            return body.keys.fold(true) { acc, key ->
-                acc && if (requestBodySnapshot[key] == null) {
-                    false
-                } else {
-                    requestBodySnapshot[key] == body[key]
-                }
-            }
-        }
+    override fun matchesSafely(request: RecordedRequest): Boolean {
+        requestBodySnapshot = parsedRequestBody.second
+        return parsedRequestBody.first == body
     }
 }
