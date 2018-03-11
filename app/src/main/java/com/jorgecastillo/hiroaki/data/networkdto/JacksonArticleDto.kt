535b51e8b0b64c50e8fcdc7e93a05b0dc9317a8f
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-03-11 17:08:49 +0100

    add jackson use case test suite

---
+++ app/src/main/java/com/jorgecastillo/hiroaki/data/networkdto/JacksonArticleDto.kt
@@ -0,0 +1,36 @@
+package com.jorgecastillo.hiroaki.data.networkdto
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.jorgecastillo.hiroaki.model.Article
+import com.jorgecastillo.hiroaki.model.Source
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class JacksonNewsResponse(
+        @JsonProperty("status") val status: String,
+        @JsonProperty("totalResults") val totalResults: Long,
+        @JsonProperty("articles") val articles: List<JacksonArticleDto>
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class JacksonArticleDto(
+        @JsonProperty("title") val title: String,
+        @JsonProperty("description") val description: String,
+        @JsonProperty("url") val url: String,
+        @JsonProperty("urlToImage") val urlToImage: String,
+        @JsonProperty("publishedAt") val publishedAt: String,
+        @JsonProperty("source") val source: JacksonSourceDto
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class JacksonSourceDto(
+        @JsonProperty("id") val id: String?,
+        @JsonProperty("name") val name: String)
+
+fun JacksonArticleDto.toArticle() = Article(title, description, url, urlToImage, publishedAt, source.toSource())
+
+fun Article.toJacksonDto() = JacksonArticleDto(title, description, url, urlToImage, publishedAt, source.toJacksonDto())
+
+fun JacksonSourceDto.toSource() = Source(id, name)
+
+fun Source.toJacksonDto() = JacksonSourceDto(id, name)
