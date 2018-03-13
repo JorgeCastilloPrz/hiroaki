571b8c64937b0511d10a0858f3bee825fda094fe
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-03-13 15:52:18 +0100

    add eq assertion for expected results2

---
+++ hiroaki/src/main/java/com/jorgecastillo/hiroaki/Assertions.kt
@@ -0,0 +1,67 @@
+package com.jorgecastillo.hiroaki
+
+import com.jorgecastillo.hiroaki.matchers.hasBody
+import com.jorgecastillo.hiroaki.matchers.hasHeaders
+import com.jorgecastillo.hiroaki.matchers.hasMethod
+import com.jorgecastillo.hiroaki.matchers.hasQueryParams
+import com.jorgecastillo.hiroaki.models.JsonBody
+import com.jorgecastillo.hiroaki.models.JsonBodyFile
+import okhttp3.mockwebserver.MockWebServer
+import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert
+import org.hamcrest.MatcherAssert.assertThat
+
+fun MockWebServer.assertRequest(
+    sentToPath: String,
+    queryParams: QueryParams? = null,
+    jsonBodyResFile: JsonBodyFile? = null,
+    jsonBody: JsonBody? = null,
+    headers: Headers? = null,
+    method: String? = null
+) {
+    throwIfBothBodyParamsArePassed(jsonBodyResFile, jsonBody)
+
+    val request = this.takeRequest()
+    MatcherAssert.assertThat(request.path, CoreMatchers.startsWith("/$sentToPath"))
+
+    queryParams?.let {
+        MatcherAssert.assertThat(request, hasQueryParams(it))
+    }
+
+    jsonBodyResFile?.let {
+        val fileStringBody = fileContentAsString(it.jsonBodyResFile)
+        MatcherAssert.assertThat(request, hasBody(
+                fileStringBody,
+                fileStringBody.fromJson(it.type),
+                request.parse(it.type)))
+    }
+
+    jsonBody?.let {
+        MatcherAssert.assertThat(request, hasBody(
+                it.jsonBody,
+                it.jsonBody.fromJson(it.type),
+                request.parse(it.type)))
+    }
+
+    headers?.let {
+        MatcherAssert.assertThat(request, hasHeaders(it))
+    }
+
+    method?.let {
+        MatcherAssert.assertThat(request, hasMethod(method))
+    }
+}
+
+private fun throwIfBothBodyParamsArePassed(
+    jsonBodyResFile: JsonBodyFile? = null,
+    jsonBody: JsonBody? = null
+) {
+    if (jsonBodyResFile != null && jsonBody != null) {
+        throw IllegalArgumentException("Please pass jsonBodyFile name or jsonBody, but not both.")
+    }
+}
+
+infix fun <T> T.eq(other: T) {
+    assertThat(this, `is`(other))
+}
