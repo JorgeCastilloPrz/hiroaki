9624021337fa024b0986579e2484d61f15faf2f7
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-03-25 16:05:17 +0200

    remove non required dependencies and enable multidex

--- app/src/main/java/com/jorgecastillo/hiroaki/SampleApp.kt
+++ app/src/main/java/com/jorgecastillo/hiroaki/SampleApp.kt
@@ -2,10 +2,11 @@ package com.jorgecastillo.hiroaki
 
 import android.app.Activity
 import android.app.Application
+import android.support.multidex.MultiDexApplication
 import com.jorgecastillo.hiroaki.data.service.MoshiNewsApiService
 import com.jorgecastillo.hiroaki.di.provideNewsService
 
-open class SampleApp : Application() {
+open class SampleApp : MultiDexApplication() {
 
     open fun newsService(): MoshiNewsApiService = provideNewsService()
 }
