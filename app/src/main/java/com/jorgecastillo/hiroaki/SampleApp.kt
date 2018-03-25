a3c1ba6cffd30f567294a4f8ae5c715a0c3906d8
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-03-25 16:06:10 +0200

    remove unused import

--- app/src/main/java/com/jorgecastillo/hiroaki/SampleApp.kt
+++ app/src/main/java/com/jorgecastillo/hiroaki/SampleApp.kt
@@ -1,7 +1,6 @@
 package com.jorgecastillo.hiroaki
 
 import android.app.Activity
-import android.app.Application
 import android.support.multidex.MultiDexApplication
 import com.jorgecastillo.hiroaki.data.service.MoshiNewsApiService
 import com.jorgecastillo.hiroaki.di.provideNewsService
