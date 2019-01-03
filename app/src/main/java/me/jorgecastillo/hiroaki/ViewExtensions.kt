d368f75e7b66c4453a9de109fb5043f874a1f10e
Jorge Castillo <jorge.castillo.prz@gmail.com>
2019-01-03 13:44:23 +0100

    update everything to androidx

--- app/src/main/java/me/jorgecastillo/hiroaki/ViewExtensions.kt
+++ app/src/main/java/me/jorgecastillo/hiroaki/ViewExtensions.kt
@@ -4,5 +4,5 @@ import android.widget.ImageView
 import com.squareup.picasso.Picasso
 
 fun ImageView.load(url: String) {
-    Picasso.with(context).load(url).into(this)
+  Picasso.get().load(url).into(this)
 }
