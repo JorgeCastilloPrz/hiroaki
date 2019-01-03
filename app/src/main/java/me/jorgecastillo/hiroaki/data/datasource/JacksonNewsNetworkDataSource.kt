42b953a51a323ed0fe66d9b3de7d3a060b9c41e8
Jorge Castillo <jorge.castillo.prz@gmail.com>
2019-01-03 13:44:23 +0100

    update everything to androidx

--- app/src/main/java/me/jorgecastillo/hiroaki/data/datasource/JacksonNewsNetworkDataSource.kt
+++ app/src/main/java/me/jorgecastillo/hiroaki/data/datasource/JacksonNewsNetworkDataSource.kt
@@ -1,11 +1,12 @@
 package me.jorgecastillo.hiroaki.data.datasource
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 import me.jorgecastillo.hiroaki.data.networkdto.toArticle
 import me.jorgecastillo.hiroaki.data.networkdto.toJacksonDto
 import me.jorgecastillo.hiroaki.data.service.JacksonNewsApiService
 import me.jorgecastillo.hiroaki.model.Article
-import kotlinx.coroutines.experimental.CommonPool
-import kotlinx.coroutines.experimental.async
 import java.io.IOException
 
 /**
@@ -13,30 +14,30 @@ import java.io.IOException
  */
 class JacksonNewsNetworkDataSource(private val service: JacksonNewsApiService) {
 
-    @Throws(IOException::class)
-    suspend fun getNews(): List<Article> {
-        val query = async(CommonPool) {
-            val response = service.getNews().execute()
-            if (response.isSuccessful) {
-                response.body()!!.articles.map { it.toArticle() }
-            } else throw IOException("Coins not found.")
-        }
-
-        return query.await()
+  @Throws(IOException::class)
+  suspend fun getNews(): List<Article> {
+    val query = GlobalScope.async(Dispatchers.IO) {
+      val response = service.getNews().execute()
+      if (response.isSuccessful) {
+        response.body()!!.articles.map { it.toArticle() }
+      } else throw IOException("Coins not found.")
     }
 
-    /**
-     * This is a no-op method on the real news API, just created for testing purposes.
-     */
-    @Throws(IOException::class)
-    suspend fun publishHeadline(article: Article) {
-        val query = async(CommonPool) {
-            val response = service.publishHeadline(article.toJacksonDto()).execute()
-            if (!response.isSuccessful) {
-                throw IOException("Coins not found.")
-            }
-        }
+    return query.await()
+  }
 
-        return query.await()
+  /**
+   * This is a no-op method on the real news API, just created for testing purposes.
+   */
+  @Throws(IOException::class)
+  suspend fun publishHeadline(article: Article) {
+    val query = GlobalScope.async(Dispatchers.IO) {
+      val response = service.publishHeadline(article.toJacksonDto()).execute()
+      if (!response.isSuccessful) {
+        throw IOException("Coins not found.")
+      }
     }
+
+    return query.await()
+  }
 }
