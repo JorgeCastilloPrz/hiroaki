f647421c54def87bc46b91fcdbd60a468809411a
Jorge Castillo <jorge.castillo.prz@gmail.com>
2019-01-03 13:36:17 +0100

    move app module dependencies to androidx

--- app/src/androidTest/java/me/jorgecastillo/hiroaki/CustomTestRunner.kt
+++ app/src/androidTest/java/me/jorgecastillo/hiroaki/CustomTestRunner.kt
@@ -2,14 +2,14 @@ package me.jorgecastillo.hiroaki
 
 import android.app.Application
 import android.content.Context
-import android.support.test.runner.AndroidJUnitRunner
+import androidx.test.runner.AndroidJUnitRunner
 
 class CustomTestRunner : AndroidJUnitRunner() {
 
-    @Throws(InstantiationException::class, IllegalAccessException::class, ClassNotFoundException::class)
-    override fun newApplication(
-        cl: ClassLoader,
-        className: String,
-        context: Context
-    ): Application = super.newApplication(cl, TestSampleApp::class.java.name, context)
+  @Throws(InstantiationException::class, IllegalAccessException::class, ClassNotFoundException::class)
+  override fun newApplication(
+      cl: ClassLoader,
+      className: String,
+      context: Context
+  ): Application = super.newApplication(cl, TestSampleApp::class.java.name, context)
 }
