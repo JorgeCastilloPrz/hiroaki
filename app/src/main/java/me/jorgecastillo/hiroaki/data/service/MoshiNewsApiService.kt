495b315fed1c2c67d9bdce2fe988fe43c40ed8f1
Jorge Castillo <jorge.castillo.prz@gmail.com>
2019-11-08 11:11:41 +0100

    Target jdk 8 with sample app.

--- app/src/main/java/me/jorgecastillo/hiroaki/data/service/MoshiNewsApiService.kt
+++ app/src/main/java/me/jorgecastillo/hiroaki/data/service/MoshiNewsApiService.kt
@@ -9,13 +9,13 @@ import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface MoshiNewsApiService {
-    @GET("/v2/top-headlines?sources=crypto-coins-news&apiKey=a7c816f57c004c49a21bd458e11e2807")
+    @GET("/v2/top-headlines?sources=crypto-coins-news&apiKey=21a12ef352b649caa97499bed2e77350")
     @Headers("Cache-Control: max-age=640000")
     fun getNews(): Call<MoshiNewsResponse>
 
     /**
      * This is a no-op method on the real news API, just created for testing purposes.
      */
-    @POST("/v2/top-headlines?apiKey=a7c816f57c004c49a21bd458e11e2807")
+    @POST("/v2/top-headlines?apiKey=21a12ef352b649caa97499bed2e77350")
     fun publishHeadline(@Body articleDto: MoshiArticleDto): Call<Void>
 }
